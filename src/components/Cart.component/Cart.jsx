312174d2a132317a4aea258a611b553bf08ee702
Abubakar Ali <abubakar@Abubakars-MacBook.local>
2021-04-26 05:53:36 +0300

    added reducer funtion to count cartItems

--- src/components/Cart.component/Cart.jsx
+++ src/components/Cart.component/Cart.jsx
@@ -4,19 +4,24 @@ import { ReactComponent as CartIcon } from '../asset/shopping-bag.svg';
 import { toggleCart } from '../../Redux/cartReducer/cartActions';
 import { connect } from 'react-redux';
 
-const Cart = ({toggleCart}) => {
+const Cart = ({ toggleCart, countItem }) => {
   return (
     <div className='cart-icon' onClick={toggleCart}>
       <CartIcon className='shopping-icon' />
-  <span className='item-count'>{}</span>
+      <span className='item-count'>{countItem}</span>
     </div>
   );
 };
 
-
-
-const mapDispatchToProps = (dispatch) => ({
-   toggleCart:() => dispatch(toggleCart())
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  countItem: cartItems.reduce(
+    (cummulated, cartItem) => cummulated + cartItem.quantity,
+    0
+  ),
 });
 
-export default connect(null,mapDispatchToProps)(Cart);
+const mapDispatchToProps = (dispatch) => ({
+  toggleCart: () => dispatch(toggleCart()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
