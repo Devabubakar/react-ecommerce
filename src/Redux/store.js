055d2275faf008b6c4ebfe213f21a1fb684316c2
Abubakar Ali <abubakarali3w@gmail.com>
2021-04-27 18:17:11 +0300

    removed logger in production

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -3,9 +3,15 @@ import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 import {persistStore} from 'redux-persist'
 
+const middleware = []
+if(process.env.NODE_ENV === "development"){
+    middleware.push(logger)
+}
 
 
-export const store = createStore(rootReducer, applyMiddleware(logger));
+
+
+export const store = createStore(rootReducer, applyMiddleware(...middleware));
 export const persistor = persistStore(store)
 
 // eslint-disable-next-line import/no-anonymous-default-export
