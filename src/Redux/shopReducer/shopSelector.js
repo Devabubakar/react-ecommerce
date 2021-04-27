218ed8d2fc12e7f2f8241359d3018aa5e3c6a10c
Abubakar Ali <abubakarali3w@gmail.com>
2021-04-27 13:42:48 +0300

    refractored shop data using data nomalization

--- src/Redux/shopReducer/shopSelector.js
+++ src/Redux/shopReducer/shopSelector.js
@@ -2,22 +2,14 @@ import { createSelector } from 'reselect';
 
 const shopSelector = (state) => state.shop;
 
-const COLLECTIONS_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
+export const selectCollections = createSelector([shopSelector], (shop) => shop.collections);
 
-export const selectShop = createSelector([shopSelector], (shop) => shop.shop);
-
-export const collectionObject = createSelector([selectShop], (collections) =>
+export const collectionObject = createSelector([selectCollections], (collections) =>
   Object.keys(collections).map((key) => collections[key])
 );
 
 export const categoryCollections = (categoryUrlParams) =>
   createSelector(
     [collectionObject],
-    (collections) => collections.find(collection => collection.id === COLLECTIONS_ID_MAP[categoryUrlParams])
+    (collections) => collections[categoryUrlParams]
   );
