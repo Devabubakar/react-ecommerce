a1a2fb71dcb316244950238b6d8283db15af8d7a
Devabubakar <abubakarali3w@gmail.com>
2021-12-04 18:28:48 +0300

    bug:fix optimazation in header

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const compression = require('compression');
 const enforce = require('express-sslify');
+const compression = require('compression');
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
@@ -10,18 +10,19 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'client/build')));
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
-app.use(compression());
 
 if (process.env.NODE_ENV === 'production') {
-  app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.get('/service-worker.js', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+  });
+
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
